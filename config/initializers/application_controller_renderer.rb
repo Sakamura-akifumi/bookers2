f7f169371c42eb877eff083c4c64a7992d5f7020
Sakamura-akifumi <git config --global user.email>
2021-09-14 07:36:26 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'bbd846517b744f4da7228fd7c441358907cd4936bb4f782d25e9c718a45c1fdfc464404d08c5138650ebe3625cd175a14e8f84bcc72d00545af5a74ef2130db9'
